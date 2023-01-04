c374960c911619f9e61c0d63f727b100af40670b
willisxiong <xiongwei.fly@gmail.com>
2023-01-04 23:01:58 +0800

    create nic and ec2 instance

--- dubai_project/main.tf
+++ dubai_project/main.tf
@@ -1,3 +1,25 @@
 provider "aws" {
     region = "ap-east-1"
 }
+
+resource "aws_instance" "sdwan_gw1" {
+  instance_type = "c5.xlarge"
+}
+
+resource "aws_network_interface" "nic_public" {
+  subnet_id = aws_subnet.public1.id
+  private_ips = ["10.1.1.1"]
+
+  tags = {
+    Name = "internet_access"
+  }
+}
+
+resource "aws_network_interface" "nic_private" {
+  subnet_id = aws_subnet.private1.id
+  private_ips = ["10.1.2.1"]
+
+  tags = {
+    Name = "mpls_access"
+  }
+}
