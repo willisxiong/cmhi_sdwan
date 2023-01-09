3952a734f28c8ed0a4c95c1d0fb8a5210b2dbc30
willisxiong <xiongwei.fly@gmail.com>
2023-01-09 19:22:29 +0800

    complete developing

--- dubai_project/main.tf
+++ dubai_project/main.tf
@@ -1,40 +1,50 @@
 provider "aws" {
-    region = "ap-east-1"
+  region = "ap-east-1"
 }
 
 resource "aws_instance" "sdwan_gw1" {
-  ami = "ami-05a3ab9cebecfeadc"
-  instance_type = "c5.xlarge"
-  key_name = "cmhi"
+  ami                         = "ami-05a3ab9cebecfeadc"
+  instance_type               = "c5.xlarge"
+  key_name                    = "cmhi_sdwan"
+  source_dest_check           = false
+  subnet_id                   = aws_subnet.private1.id
+  associate_public_ip_address = false
 }
 
 
 resource "aws_network_interface" "nic_public" {
-  subnet_id = aws_subnet.public1.id
-  private_ips = ["10.1.2.1"]
+  subnet_id   = aws_subnet.public1.id
+  private_ips = ["10.1.1.10"]
+  security_groups = [aws_security_group.sdwan_sg.id]
 
   tags = {
     Name = "internet_access"
   }
 }
 
-resource "aws_network_interface" "nic_private" {
-  subnet_id = aws_subnet.private1.id
-  private_ips = ["10.1.1.1"]
-
-  tags = {
-    Name = "mpls_access"
-  }
-}
-
 resource "aws_network_interface_attachment" "internet" {
-  instance_id = aws_instance.sdwan_gw1.id
+  instance_id          = aws_instance.sdwan_gw1.id
   network_interface_id = aws_network_interface.nic_public.id
-  device_index = 0
+  device_index         = 1
 }
 
-resource "aws_network_interface_attachment" "mpls" {
-  instance_id = aws_instance.sdwan_gw1.id
-  network_interface_id = aws_network_interface.nic_private.id
-  device_index = 0
+resource "aws_security_group" "sdwan_sg" {
+  name        = "cmhi_sdwan_internet_sg"
+  description = "allow internet traffic"
+  vpc_id      = aws_vpc.cmhi_vpc_1.id
+
+  ingress {
+    description = "allow ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
--- dubai_project/vpc.tf
+++ dubai_project/vpc.tf
@@ -1,9 +1,9 @@
 resource "aws_vpc" "cmhi_vpc_1" {
-    cidr_block = "10.1.0.0/16"
+  cidr_block = "10.1.0.0/16"
 
-    tags = {
-        Name = "cmhi_dubai_1"
-    }
+  tags = {
+    Name = "cmhi_dubai_1"
+  }
 }
 
 resource "aws_route_table" "sdwan_rt" {
@@ -19,22 +19,32 @@ resource "aws_route_table" "sdwan_rt" {
   }
 }
 
-resource "aws_subnet" "public1" {
-    vpc_id = aws_vpc.cmhi_vpc_1.id
-    cidr_block = "10.1.1.0/24"
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.private1.id
+  route_table_id = aws_route_table.sdwan_rt.id
+}
 
-    tags = {
-        Name = "pub_subnet"
-    }
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.sdwan_rt.id
+}
+
+resource "aws_subnet" "public1" {
+  vpc_id     = aws_vpc.cmhi_vpc_1.id
+  cidr_block = "10.1.1.0/24"
+
+  tags = {
+    Name = "pub_subnet"
+  }
 }
 
 resource "aws_subnet" "private1" {
-    vpc_id = aws_vpc.cmhi_vpc_1.id
-    cidr_block = "10.1.2.0/24"
+  vpc_id     = aws_vpc.cmhi_vpc_1.id
+  cidr_block = "10.1.2.0/24"
 
-    tags = {
-        Name = "pri_subnet"
-    }
+  tags = {
+    Name = "pri_subnet"
+  }
 }
 
 resource "aws_vpn_gateway" "vgw" {
@@ -46,7 +56,7 @@ resource "aws_vpn_gateway" "vgw" {
 }
 
 resource "aws_vpn_gateway_attachment" "vgw_attachment" {
-  vpc_id = aws_vpc.cmhi_vpc_1.id
+  vpc_id         = aws_vpc.cmhi_vpc_1.id
   vpn_gateway_id = aws_vpn_gateway.vgw.id
 }
 
@@ -59,7 +69,7 @@ resource "aws_internet_gateway" "igw" {
 }
 
 resource "aws_eip" "eip_cmhi" {
-  vpc = true
-  network_interface = aws_network_interface.nic_public.id
-  associate_with_private_ip = "10.1.2.1"
+  vpc                       = true
+  network_interface         = aws_network_interface.nic_public.id
+  associate_with_private_ip = "10.1.1.10"
 }
